5ad028f513f04dba49ebdca36f6bf530c03d4763
sergio-jc <sergiojara0609@gmail.com>
2022-10-31 00:50:36 -0500

    feat(client/src/pages): add  user favorites notRegisteredUser

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,30 @@ import { Home } from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Detail } from "./pages/Detail";
 import { NavBar } from "./components/NavBar";
+import { Favs } from "./pages/Favs";
+import { User } from "./pages/User";
+import { useState } from "react";
+import { NotRegisteredUser } from "./pages/NotRegisteredUser";
 
 export default function App() {
+    const [isLogged, setIsLogged] = useState(true);
     return (
         <BrowserRouter>
             <Logo />
-
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/pet/:id" element={<Home />} />
-                <Route path="/detail/:detailId" element={<Detail/>}/>
+                <Route path="/detail/:detailId" element={<Detail />} />
+                <Route
+                    path="/favorites"
+                    element={isLogged ? <Favs /> : <NotRegisteredUser />}
+                />
+                <Route
+                    path="/user"
+                    element={isLogged ? <User /> : <NotRegisteredUser />}
+                />
             </Routes>
-            <NavBar/>
+            <NavBar />
         </BrowserRouter>
     );
 }
---
+++ client/src/pages/NotRegisteredUser.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NotRegisteredUser = () => {
+  return (
+    <div>NotRegisteredUser</div>
+  )
+}
